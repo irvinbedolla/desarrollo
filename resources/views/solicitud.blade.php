edce9ebb0b7822fe29113289a845ef084d7249e5
AR_Leal <anarosalm@outlook.com>
2025-03-19 12:19:57 -0600

    actualización de logos para redes sociales

--- resources/views/solicitud.blade.php
+++ resources/views/solicitud.blade.php
@@ -105,11 +105,12 @@
                 <span class="mb-3 mb-md-0 text-body-secondary">Teléfono de contacto: <b>(443) 688 6337</b></span>
             </div>
             <div class="col-md-4 d-flex align-items-center">
-              <div class="col-md-3, mx-auto, my-auto">
-                <a href="https://x.com/cclaboralmich"><img src="public/assets_seer/images/x-logo.png" style="width: 8%"></a>
-                &ensp;<a href="https://www.instagram.com/cclmichoacan/"><img src="public/assets_seer/images/instagram.png" style="width:10%"></a>
-                &ensp;<a href="https://www.facebook.com/conciliacionlaboralmich/?locale=es_LA"><img src="public/assets_seer/images/face.png" style="width:8%"></a>
-              </div>
+                <div class="col-md-3, mx-auto, my-auto">
+                    <a href="https://x.com/cclaboralmich"><img src="public/assets_seer/images/X.png" style="width: 8%"></a>
+                    &ensp;<a href="https://www.instagram.com/cclmichoacan/"><img src="public/assets_seer/images/IG.png" style="width:10%"></a>
+                    &ensp;<a href="https://www.facebook.com/conciliacionlaboralmich/?locale=es_LA"><img src="public/assets_seer/images/FC.png" style="width:8%"></a>
+                    &ensp;<a href="https://www.tiktok.com/@cclmichoacan0?_t=ZM-8uooi2eSI1V&_r=1"><img src="public/assets_seer/images/TK.png" style="width:8%"></a>
+                </div>
             </div>
         </footer>
       
