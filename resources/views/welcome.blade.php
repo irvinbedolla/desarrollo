ca610f1605627f917412350e178ca961e63546b0
ofeliaTR <otapiarodriguez@gmail.com>
2025-03-20 16:32:35 -0600

    ruta de x

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -91,8 +91,8 @@
     </div>
     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style="">
       <ol class="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="0"class="active"></li>
+        <!--<li data-target="#carouselExampleIndicators" data-slide-to="1" class="active"></li>-->
       </ol>
       <div class="carousel-inner">
        <!--<div class="carousel-item">
@@ -256,7 +256,7 @@
       </div>
       <div class="col-md-4 d-flex align-items-center">
         <div class="col-md-3, mx-auto, my-auto">
-          <a href="https://x.com/cclmichoacan/status/1902452234568265892"><img src="public/assets_seer/images/X.png" style="width: 8%"></a>
+          <a href="https://x.com/cclmichoacan?s=11"><img src="public/assets_seer/images/X.png" style="width: 8%"></a>
           &ensp;<a href="https://www.instagram.com/cclmichoacan/"><img src="public/assets_seer/images/IG.png" style="width:10%"></a>
           &ensp;<a href="https://www.facebook.com/conciliacionlaboralmich/?locale=es_LA"><img src="public/assets_seer/images/FC.png" style="width:8%"></a>
           &ensp;<a href="https://www.tiktok.com/@cclmichoacan0?_t=ZM-8uooi2eSI1V&_r=1"><img src="public/assets_seer/images/TK.png" style="width:8%"></a>
