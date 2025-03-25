280206ad12def6dbbdfcc4e68adc10f2cbf44c36
ofeliaTR <otapiarodriguez@gmail.com>
2025-03-25 11:55:54 -0600

    cammbio de imagen en carrucel y cambio de logos menu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,9 +10,7 @@
         <title>Si Conciliación</title>
         <!-- Bootstrap core CSS -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        
         <link rel="icon" href="public/assets_seer/images/logo-ccl.png" type="image/x-icon">
         <style>
           body{
@@ -63,8 +61,8 @@
     <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
         <div class="">
-            <img src="public/assets_seer/images/Logo1.jpeg" class="img" style="" width="150" height="70"></a>&nbsp;&nbsp;
-            <img src="public/assets_seer/images/logo2.jpeg" class="img" style="" width="90" height="90"></a>
+            <img src="public/assets_seer/images/Logo2.jpeg" class="img" style="" width="90" height="90"></a>&nbsp;&nbsp;
+            <img src="public/assets_seer/images/logo1.jpeg" class="img" style="" width="150" height="70"></a>
         </div>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -89,17 +87,18 @@
     <div class="container">
           <br><br><br><br>
     </div>
+    
     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style="">
       <ol class="carousel-indicators">
         <li data-target="#carouselExampleIndicators" data-slide-to="0"class="active"></li>
         <!--<li data-target="#carouselExampleIndicators" data-slide-to="1" class="active"></li>-->
       </ol>
       <div class="carousel-inner">
-       <!--<div class="carousel-item">
-          <img class="d-block  w-100 vh-100" src="public/assets_seer/images/carusel/2.png" alt="Second slide" style="width:100%">
+        <!--<div class="carousel-item">
+          <img class="d-block  w-100 vh-100" src="public/assets_seer/images/carusel/GM.png" alt="Second slide" style="width:100%">
         </div>-->
         <div class="carousel-item active">
-          <img class="d-block  w-100 vh-100" src="public/assets_seer/images/carusel/3.png" alt="Third slide" style="width:100%">
+          <img class="d-block  w-100 vh-100" src="public/assets_seer/images/carusel/1Portada-web.png" alt="Third slide" style="width:100%">
         </div>
       </div>
       <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -265,7 +264,10 @@
     </footer> 
 
       <!--<script src="public/assets_seer/assets/dist/js/bootstrap.bundle.min.js"></script>-->
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+      <!--<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>-->
     </body>
 </html>
 
