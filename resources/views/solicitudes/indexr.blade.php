a12164d27a73eecafb7322d493fb400787890876
AR_Leal <anarosalm@outlook.com>
2025-07-09 15:01:29 -0600

    actualización

--- resources/views/solicitudes/indexr.blade.php
+++ resources/views/solicitudes/indexr.blade.php
@@ -74,7 +74,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Motivo de rechazo</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Motivo</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
