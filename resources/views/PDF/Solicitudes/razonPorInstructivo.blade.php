c004b7739f8f5313acb7d6868845eb0f282c6c37
irvin_bedolla <irvinsbm@gmail.com>
2025-08-11 14:02:36 -0600

    actualizaciòn documento notificadores

--- resources/views/PDF/Solicitudes/razonPorInstructivo.blade.php
+++ resources/views/PDF/Solicitudes/razonPorInstructivo.blade.php
@@ -82,41 +82,49 @@
                                 <td><b>Centro de conciliación: </b></td>
                                 <td>{{ $solicitud->delegacion }}</td>
                             </tr>
+                            <tr>   
+                                <td><b>Solicitante: </b></td>
+                                <td>{{$solicitante->nombre}}</td>
+                            </tr>
+                             <tr>   
+                                <td><b>Citado: </b></td>
+                                <td>{{$citado->nombre}} {{$citado->primer_apellido}} {{$citado->segundo_apellido}}</td>
+                            </tr>
                     </table>
                 </div><br><br><br>
 
-                <p><center><b>RAZÓN DE NOTIFICACIÓN POR INSTRUCTIVO</b></center></p><br>
-                <p><b>
-                      SOLICITANTE: {{$solicitante->nombre}}<br>
-                      CITADO: {{$citado->nombre}} {{$citado->primer_apellido}} {{$citado->segundo_apellido}}
-                </b></p>  
+                <p><center><b>RAZÓN DE NOTIFICACIÓN POR INSTRUCTIVO</b></center></p><br> 
                            
                 <p>Siendo las <b>{{ \Carbon\Carbon::now()->format('H') }} HORAS CON {{ \Carbon\Carbon::now()->format('i') }} MINUTOS
                     DEL DÍA {{ \Carbon\Carbon::now()->translatedFormat('d \d\e F \d\e\l Y') }}, LIC. {{$notificador->name}}</b> en mi
-                    calidad de notificador adscrito al Centro de Conciliación Laboral, oficina estatal {{ $solicitud->delegacion }}, a efecto de dar cumplimiento al CITATORIO DE CONCILIACIÓN
-                    de fecha <b>{{ \Carbon\Carbon::parse($solicitud->fecha)->translatedFormat('d \d\e F \d\e\l Y') }}</b> en el expediente citado, en el que se ordena NOTIFICAR 
-                    <b>AL CITADO: {{$citado->nombre}} {{$citado->primer_apellido}} {{$citado->segundo_apellido}}</b>, en el domicilio señalado
-                    en <b>{{$citado->tipo_vialidad}} {{$citado->calle}} {{$citado->n_ext}}@if($citado->n_int!=null) int. {{$citado->n_int}}@endif, COLONIA {{$citado->colonia}}, {{$municipioCitado}}, CP {{$citado->cp}}, 
+                    calidad de notificador adscrito al Centro de Conciliación Laboral, oficina estatal {{ $solicitud->delegacion }}, en ejercicio de las facultades conferidas en los artìculos de la Ley Orgànica del 
+                    Centro de Conciliaciòn Laboral del Estado de Michoacàn de Ocampo y 21 del reglamento interior del Centro de Conciliaciòn Laboral del Estado de Michoacàn de Ocampo, a efecto de dar cumplimiento 
+                    al CITATORIO DE CONCILIACIÓN de fecha <b>{{ \Carbon\Carbon::parse($solicitud->fecha)->translatedFormat('d \d\e F \d\e\l Y') }}</b> en el expediente citado al rubro, en el que se ordena NOTIFICAR 
+                    <b>AL CITADO: {{$citado->nombre}}@if($citado->primer_apellido!=null) {{$citado->primer_apellido}}@endif @if($citado->segundo_apellido!=null) {{$citado->segundo_apellido}}@endif</b>, en el domicilio señalado
+                    en <b>{{strtoupper($citado->tipo_vialidad)}} {{$citado->calle}} {{$citado->n_ext}}@if($citado->n_int!=null) int. {{$citado->n_int}}@endif, COLONIA {{$citado->colonia}}, {{$municipioCitado}}, CP {{$citado->cp}}, 
                     ESTADO MICHOACÁN DE OCAMPO</b><br><br>
 
-                    Cerciorándome de ser éstos los Municipio, Colonia y Vialidad correctas señaladas en la solicitude de conciliación, por
+                    Cerciorándome de ser éstos los Municipio, Colonia y Vialidad correctas señaladas en la solicitud de Conciliación, apegàndome a los siguientes elementos de convicciòn
                     <b>{{$citado->abundar_area}} a) LA(S) PLACAS DE SEÑALIZACIÓN OFICIAL MÁS PRÓXIMA(S) AL DOMICILIO EN QUE SE ACTÚA, CON EL RESPECTIVO NOMBRE DE LA ALCALDÍA, COLONIA Y CALLE, 
                     b) EL MÚMERO VISIBLE DEL INMUEBLE, c) EL NÚMERO DEL INMUEBLE ES CONSISTENTE CON LA NUMERACIÓN DE LA CALLE, Y d) LOS INFORMES DE VECINOS DEL LUGAR, 
                     QUIENES CONFIRMAN QUE SE TRATA DEL DOMICILIO CORRECTO. A mayor abundamiento, verifico que cerca del domicilio se encuentran los siguientes puntos  
                     de referencia: {{$citado->abundar_inmueble}} A SU COSTADO DERECHO SE ENCUNTRA EL INMUEBLE CON EL NÚMERO 218. De igual forma, he constatado que se trata de un inmueble con las 
                     siguientes características: CONSTA DE PLANTA BAJA Y UN PISO, CON FACHADA EN COLOR GRIS CON PORTÓN DE ACCESO EM SOLOR GRIS</b>.<br><br>
 
-                    Procedí a tocar en repetidas ocasiones, sin recibir respuesta. Y después de haber esperado un tiempo prudente, lógico y razonable, nadie acude a mi llamado, sin embargo cuento con 
-                    los medios de cercioramiento antes mencionados de que el domicilio es el correcto y que el citado labora, habita o tiene su asiento de negocios 
-                    en este domicilio con base en <b>{{$citado->observaciones}} ME CONSTITUÍ EN LEGAL Y DEBIDA FORMAEN EL DOMICILIO SEÑALADO EN EL CITATORIO A REFERENCIA, EN DONDE DESPUÉS DE UBICAR 
+                    Procedí a tocar y fui atendido por una persona, quièn manifestò ser mayor de edad, procedì a solicitarle que me proporcionara su nombre y una identificaciòn oficial, pero se negò a hacerlo. Procedo a 
+                    especificar su media filiaciòn, que contiene los siguientes rasgos: <b>SEXO {{$citado->genero}}, TEZ {{$citado->tez}}, EDAD {{$citado->edad_filiacion}} AÑOS, ALTURA {{$citado->altura}} M.,
+                    COMPLEXIÓN {{$citado->complexion}}, CABELLO {{$citado->cabello}}, OJOS {{$citado->ojos}} Y SEÑAS PARTICULARES: {{$citado->particulares}}. LO ANTERIOR SE HACE DE
+                    MANERA APROXIMADA, YA QUE EL SUSCRITO NO ES PERITO EN LA MATERIA</b>. Sin embargo, cuento con los medios de cercioramiento antes mencionados de que el domicilio es el correcto y que el citado labora, 
+                    habita o tiene su asiento de negocios en este domicilio con base en que 
+                    <b>{{$citado->observaciones}} ME CONSTITUÍ EN LEGAL Y DEBIDA FORMA EN EL DOMICILIO SEÑALADO EN EL CITATORIO A REFERENCIA, EN DONDE DESPUÉS DE UBICAR 
                     EL NÚMERO 90 PROCEDÍ A TOCAR LA PUERTA EN REPETIDAS OCASIONES, SIN EMBARGO NADIE ACUDIÓ A MI LLAMADO; PREGUNTÉ CON LOS VECINOS DE AL LADO, QUIENES 
                     CONFIRMAN QUE EFECTIVAMENTE LA CITADA OCUPA EL INMUEBLE, POR LO QUE PROCEDO A NOTIFICAR EL CITATORIO POR INSTRUCTIVO</b>.<br><br>
 
-                    Por todo lo anterior en términos de lo previsto en los artículos 741, 742 fracción XIII, 743 y 751 de la Ley Federal del Trabajo procedo a notificar 
+                    Por todo lo anterior en términos de lo previsto en los artículos 741, 742 fracción XIII, 743 fracciòn V y 751 de la Ley Federal del Trabajo procedo a notificar 
                     por instructivo pegando <b>CITATORIO</b> Y CÉDULA DE LEY POR INSTRUCTIVO en la puerta de entrada del domicilio.<br><br>
 
                     <b>Anexando impresión fotográfica para constancia legal.<br>
-                    Doy cuenta a la autoridad conciliadora competente y lo hago constar para todos los efectos legales a que haya lugar. DOY FE.</b> 
+                    Doy cuenta a la autoridad conciliadora competente y lo hago constar para todos los efectos legales a que haya lugar. Doy fe.</b> 
                 </p>
                 <br>
                 <p><center><b>___________________________________<br>LIC. {{$notificador->name}} <br> FUNCIONARIO/A NOTIFICADOR/A</b></center></p>
