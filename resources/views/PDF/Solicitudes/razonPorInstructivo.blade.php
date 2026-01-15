34c7bd32d0f8347a3277a0ef0400f39757e7a2c7
AR_Leal <anarosalm@outlook.com>
2026-01-15 15:08:42 -0600

    actualización citatorio y notificación por instructivo

--- resources/views/PDF/Solicitudes/razonPorInstructivo.blade.php
+++ resources/views/PDF/Solicitudes/razonPorInstructivo.blade.php
@@ -134,7 +134,7 @@
                     <p>Siendo las <b>{{ $citado->updated_at->format('H') }} HORAS CON {{ $citado->updated_at->format('i') }} MINUTOS
                         DEL DÍA {{ $citado->updated_at->translatedFormat('d \d\e F \d\e\l Y') }}, LIC. {{$notificador->name}}</b> en mi
                         calidad de notificador(a) adscrito al Centro de Conciliación Laboral, oficina estatal {{ $solicitud->delegacion }}, me constituyo física y legalmente en el domicilio ubicado en
-                        <b>{{strtoupper($citado->tipo_vialidad)}} {{$citado->calle}} {{$citado->n_ext}}@if($citado->n_int!=null) INT. {{$citado->n_int}}@endif, COLONIA {{$citado->colonia}}, {{strtoupper($municipioCitado, 'UTF-8')}}, CP {{$citado->cp}}, 
+                        <b>{{mb_strtoupper($citado->tipo_vialidad, 'UTF-8')}} {{$citado->calle}} {{$citado->n_ext}}@if($citado->n_int!=null) INT. {{$citado->n_int}}@endif, COLONIA {{$citado->colonia}}, {{mb_strtoupper($municipioCitado, 'UTF-8')}}, CP {{$citado->cp}}, 
                         {{mb_strtoupper($estadoCitado, 'UTF-8')}}</b>, siendo este el domicilio señalado en la solicitud de conciliación como el del <b>CITADO: {{$citado->nombre}}@if($citado->primer_apellido!=null) {{$citado->primer_apellido}}@endif @if($citado->segundo_apellido!=null) {{$citado->segundo_apellido}}@endif</b>
                         Todo ello a efecto de dar cumplimiento al <b>CITATORIO DE CONCILIACIÓN</b> de fecha <b>{{ \Carbon\Carbon::parse($solicitud->fecha)->translatedFormat('d \d\e F \d\e\l Y') }}</b> en el expediente citado. Y cerciorando de ser 
                         este el domicilio correcto y completo, apegándome en los siguientes elementos de convicción: 
