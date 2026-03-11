be3137d23c0d705b51507d0d9c3a73db42e3d96e
AR_Leal <anarosalm@outlook.com>
2026-03-11 16:16:42 -0600

    actualizaciones para el foro

--- resources/views/emails/foro.blade.php
+++ resources/views/emails/foro.blade.php
@@ -4,10 +4,10 @@
     <title>Bienvenido(a)</title>
 </head>
 <body>
-    <h1>Hola, {{ $userData['nombre'] }} {{ $userData['primer_apellido'] }} {{ $userData['segundo_apellido'] }}!</h1>
-    ¡Gracias por registrarte para el Foro Nacional por la Consolidación de la Justicia Laboral en México!
+    <h1>Hola, {{ $userData['nombre'] }} {{ $userData['primer_apellido'] }} {{ $userData['segundo_apellido'] }}</h1>
+    ¡Gracias por registrarte al Foro Nacional por la Consolidación de la Justicia Laboral en México!.
 
-    Podrás obtener tu constancia de participación a través de este enlace, el cual se activará a partir del lunes 16 de marzo.  
+    Podrás obtener tu constancia de participación a partir del lunes 16 de marzo.  
 
     Conoce todos los detalles del evento en el sitio: https://foro-nacional.cclmichoacan.gob.mx/
 </body>
