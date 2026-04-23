25eb692772db3f70da012f69c7b1235b6d1d6f3f
irvin_bedolla <irvinsbm@gmail.com>
2026-04-23 15:43:47 -0600

    Propuesta para las asistencias por QR

---
+++ resources/views/generaQR.blade.php
@@ -0,0 +1,7 @@
+<div class="card">
+    <h1>Tu Código de Asistencia</h1>
+    <div class="qr-container">
+        {!! $codigoQR !!}
+    </div>
+    <p>Escanea este código en la tablet al llegar.</p>
+</div>
