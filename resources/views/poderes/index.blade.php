ac8fa48a27e80249da2df1fb374cf7d3d077ef30
AR_Leal <anarosalm@outlook.com>
2026-04-30 12:51:33 -0600

    corrección de rutas al visualizar los documentos de poderes

--- resources/views/poderes/index.blade.php
+++ resources/views/poderes/index.blade.php
@@ -94,7 +94,7 @@
                                                         @endif
                                                     </td>
                                                     <td>{{$persona->estatus}}</td>
-                                                    <td><a target="_blank" href="../storage/app/documentos_abogados/{{$persona->ineDocumento}}">PDF</a></td>
+                                                    {{--<td><a target="_blank" href="../storage/app/documentos_abogados/{{$persona->ineDocumento}}">PDF</a></td> 
                                                     <td>
                                                         @if($persona->cedulaDocumento == NULL)
                                                             S/D
@@ -114,6 +114,29 @@
                                                             S/A
                                                         @else
                                                             <a target='_blank' href='../storage/app/documentos_abogados/{{$persona->cedulaDocumento}}'>PDF</a>
+                                                        @endif
+                                                    </td>--}}
+                                                    <td><a target="_blank" href="../storage/app/documentos_abogados/{{$persona->ineDocumento}}">PDF</a></td> 
+                                        
+                                                    <td>
+                                                        @if($persona->representacionDocumento == NULL)
+                                                            S/D
+                                                        @else 
+                                                            <a target='_blank' href='../storage/app/documentos_abogados/{{$persona->representacionDocumento}}'>PDF</a>
+                                                        @endif
+                                                    </td>
+                                                    <td>
+                                                        @if($persona->cedulaDocumento == NULL)
+                                                            S/D
+                                                        @else
+                                                            <a target="_blank" href="../storage/app/documentos_abogados/{{$persona->cedulaDocumento}}">PDF</a>
+                                                        @endif
+                                                    </td>
+                                                    <td>
+                                                        @if($persona->cedula === "Sin carta poder")
+                                                            S/D
+                                                        @else
+                                                            <a target='_blank' href='../storage/app/documentos_abogados/{{$persona->anexo_documento}}'>PDF</a>
                                                         @endif
                                                     </td>
                                                     <td>
