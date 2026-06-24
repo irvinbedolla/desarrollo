e6e83ff1e5f723fe2df86295e3bf636a1e9e9b19
irvin_bedolla <irvinsbm@gmail.com>
2026-06-24 16:02:43 -0600

    Actualizacion de ruta

--- routes/web.php
+++ routes/web.php
@@ -535,6 +535,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
         Route::get('/cumplimiento/PDFIncumplimientoR/{id}',             [TurnosController::class, 'PDFincumplimientoRatificacion'])->name('PDFincumplimientoRatificacion');
         Route::get('/VerpdfcPTUNLaboraRat/{id}',                        [TurnosController::class, 'VerPDFConvenioPTU_rat'])->name('PDFconvenioPTU_NO_R');
         Route::get('/Verpdf/{id}',                                      [TurnosController::class, 'VerPDF'])->name('PDFratifi');
+        Route::get('/Verpdfaudiencia/{id}',                             [TurnosController::class, 'VerPDFAudiencia'])->name('PDFaudiencia');
         
         Route::get('/ratificaciones/vista_previaCitas/{id_solicitud}',  [TurnosController::class, 'vista_previa_citas'])->name('vista_previa_citas'); //Vista previa de la vista citas(primera parte del llenado de la ratificación)
         Route::post('/ratificaciones/guardarEdicion_citas',             [TurnosController::class, 'guardarEdicion_citas'])->name('guardarEdicion_citas');
